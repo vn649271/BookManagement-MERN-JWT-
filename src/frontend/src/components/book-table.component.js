f9b2a7571474c0a2fac01b7b42cbf84444b14600
Cowboy <vn649271@gmail.com>
2022-12-05 10:39:28 +0200

    - Introduced delete button with confirm dialog

--- src/frontend/src/components/book-table.component.js
+++ src/frontend/src/components/book-table.component.js
@@ -7,55 +7,70 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { FaPencilAlt, FaUserAlt, FaTrashAlt, FaRegSave } from "react-icons/fa";
+import DeleteButton from './delete-button.component';
 
 export default function BookTable(props) {
 	const { data, onDelete } = props;
 
+	const [open, setOpen] = React.useState(false);
+	const [selectedValue, setSelectedValue] = React.useState("");
+
+
 	const onClickEdit = (row) => {
 		console.log(row)
 	}
-	const onClickDelete = (row) => {
+	const onClickDelete = (ret, row) => {
+		if (!ret) {
+			return;
+		}
 		onDelete(row._id)
 	}
   	return (
-    	<TableContainer component={Paper}>
-			<Table sx={{ minWidth: 650 }} aria-label="simple table">
-				<TableHead>
-					<TableRow>
-						<TableCell>Title</TableCell>
-						<TableCell>Description</TableCell>
-						<TableCell>Author</TableCell>
-						<TableCell align="center">Published At</TableCell>
-						<TableCell align="right"></TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-				{
-					data.length > 0 ?
-					(data.map((row, index) => (
-					    <TableRow
-					      key={"book-" + index}
-					      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-					    >
-					    	<TableCell component="th" scope="row">{row.title}</TableCell>
-						    <TableCell>{row.description}</TableCell>
-						    <TableCell>{row.authorName}</TableCell>
-						    <TableCell align="center">{row.published_at.replace("T", " ").replace("Z", "").replace(/\.[0-9]+/, "")}</TableCell>
-						    <TableCell align="right">
-						    	<FaPencilAlt className="cursor-pointer" onClick={() => onClickEdit(row)} />
-						    	<span className="pr-3"></span>
-						    	<FaTrashAlt className="cursor-pointer" onClick={() => onClickDelete(row)} />
-						    </TableCell>
-					    </TableRow>
-					))):
-					<TableRow 
-					    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-					>
-				    	<TableCell className="text-center" colSpan="5">No data</TableCell>
-					</TableRow>
-				}
-				</TableBody>
-			</Table>
-	    </TableContainer>
+  		<>
+	    	<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Title</TableCell>
+							<TableCell>Description</TableCell>
+							<TableCell>Author</TableCell>
+							<TableCell align="center">Published At</TableCell>
+							<TableCell align="right"></TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+					{
+						data.length > 0 ?
+						(data.map((row, index) => (
+						    <TableRow
+						      key={"book-" + index}
+						      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+						    >
+						    	<TableCell style={{width: '100px'}} component="th" scope="row">{row.title}</TableCell>
+							    <TableCell style={{'max-width': '400px', overflow: 'hidden'}}>{row.description}</TableCell>
+							    <TableCell style={{'max-width': '100px', overflow: 'hidden'}}>{row.authorName}</TableCell>
+							    <TableCell align="center">{row.published_at.replace("T", " ").replace("Z", "").replace(/\.[0-9]+/, "")}</TableCell>
+							    <TableCell align="right">
+							    	<div className="row">
+							    		<div className="md-col-6">
+									    	<FaPencilAlt className="cursor-pointer" onClick={() => onClickEdit(row)} />
+							    		</div>
+							    		<div className="md-col-6">
+									    	<DeleteButton className="cursor-pointer" onDelete={(ret) => onClickDelete(ret, row)} />
+							    		</div>
+							    	</div>
+							    </TableCell>
+						    </TableRow>
+						))):
+						<TableRow 
+						    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+						>
+					    	<TableCell className="text-center" colSpan="5">No data</TableCell>
+						</TableRow>
+					}
+					</TableBody>
+				</Table>
+		    </TableContainer>
+		</>
   );
 }
---
+++ src/frontend/src/components/delete-button.component.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { FaPencilAlt, FaUserAlt, FaTrashAlt, FaRegSave } from "react-icons/fa";
+
+export default function DeleteButton(props) {
+
+	const { onDelete } = props;
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (ret) => {
+    setOpen(false);
+    onDelete(ret);
+  };
+
+  return (
+    <div>
+    	<FaTrashAlt className="cursor-pointer" onClick={handleClickOpen} />
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Confirm"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure to delete?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleClose(0)}>Cancel</Button>
+          <Button onClick={() => handleClose(1)} autoFocus>
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
